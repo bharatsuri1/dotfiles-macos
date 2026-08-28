b61303de34f15b99ba89febc6b085297c978e955
Bharat Suri <bharatsuri.us@gmail.com>
2026-08-27 18:13:59 -0700

    Use sesh for the home session

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -20,6 +20,11 @@ alias hidefiles='defaults write com.apple.finder AppleShowAllFiles false && kill
 t() {
   local session_name="${1:-home}"
 
+  if [[ "$session_name" == "home" ]]; then
+    sesh connect home
+    return
+  fi
+
   if [[ -n "$TMUX" ]]; then
     tmux has-session -t "=$session_name" 2>/dev/null ||
       tmux new-session -d -s "$session_name" -c "$HOME"
